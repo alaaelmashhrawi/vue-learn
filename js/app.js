81d742c18a8d08f264fb299c90f7bc6f3536d707
Alaa Abd <alaa.abd.mash@gmail.com>
2019-12-12 14:05:20 +0200

    “events“

--- js/app.js
+++ js/app.js
@@ -1,17 +1,34 @@
 new Vue({
-    el:"#vue-app",
-    data :{
-        name :'alaa elmashhrawi' ,
-        age :'30' ,
-        job :'front end developer',
-        website:'www.google.com',
-        websitetag: '<a href="www.google.com">this html link</a>'
+    el: "#vue-app",
+    data: {
+        name: 'alaa elmashhrawi',
+        age: '30',
+        job: 'front end developer',
+        website: 'www.google.com',
+        websitetag: '<a href="www.google.com">this html link</a>',
+        y:0,
+        x:0
 
 
     },
-    methods:{
-        greet : function(time){
-        return 'good ' + time +' '+ this.name;
+    methods: {
+        greet: function (time) {
+            return 'good ' + time + ' ' + this.name;
+        },
+        add: function (inec) {
+            this.age +=inec;
+
+        },
+        subtract: function(dec) {
+            this.age -=dec;
+
+        },
+        postion: function(event) {
+           console.log(event);
+           this.x= event.offsetX;
+           this.y= event.offsetY;
+
+
+        }
     }
-}
 });
